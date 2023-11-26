c87a46e9755e9d5d72f0da4dab45e674b8b68c87
sreekanth-macherla-azure <114579018+sreekanth-macherla-azure@users.noreply.github.com>
2023-11-26 12:55:13 -0800

    Add files via upload

---
+++ GKECLuster.tf
@@ -0,0 +1,218 @@
+
+provider  "google" {
+    project = "kubernetes-403103"
+    region = "us-central1"
+}
+
+
+terraform {
+   # backend "gcs" {
+
+   #     bucket = "terraformsree/terraformGKE"
+    #    prefix = "terraform/state"
+             
+  # }
+required_providers {
+  google ={
+    source = "hashicorp/google"
+    version = "~>4.0"
+     }
+}
+}
+
+resource "google_project_service" "compute" {
+    service = "compute.googleapis.com"
+  
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_compute_network" "main" {
+    name = "gcnetwork"
+    routing_mode = "REGIONAL"
+    auto_create_subnetworks = false
+    mtu = 1460
+    delete_default_routes_on_create = false
+
+    depends_on = [ 
+        google_project_service.compute,
+        google_project_service.container
+     ]
+  
+}
+
+#Create Subnet in GCP using Terraform
+
+resource "google_compute_subnetwork" "private" {
+    name = "private"
+    ip_cidr_range   = "10.0.0.0/18"
+    region = "us-central1"
+    network = google_compute_network.main.id
+    private_ip_google_access = true
+
+secondary_ip_range  {
+    range_name = "k8s-pod-range"
+    ip_cidr_range = "10.48.0.0/14"
+}
+secondary_ip_range  {
+    range_name = "k8s-service-range"
+    ip_cidr_range = "10.52.0.0/20"
+}
+
+}
+
+resource "google_compute_router" "router" {
+  name    = "router"
+  region  = "us-central1"
+  network = google_compute_network.main.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  router = google_compute_router.router.name
+  region = "us-central1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.nat.self_link]
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_address
+resource "google_compute_address" "nat" {
+  name         = "nat"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+
+  depends_on = [google_project_service.compute]
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_container_cluster" "primary" {
+
+    name    = "kubeenvtest"
+    location = "us-central1-a"
+    remove_default_node_pool = true
+    initial_node_count = 1
+    network = google_compute_network.main.name
+    subnetwork = google_compute_subnetwork.private.self_link
+    logging_service = "logging.googleapis.com/kubernetes"
+    monitoring_service       = "monitoring.googleapis.com/kubernetes"
+    networking_mode          = "VPC_NATIVE"
+ addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  workload_identity_config {
+    workload_pool = "kubernetes-403103.svc.id.goog"
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pod-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+  
+}
+
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes"
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
+resource "google_container_node_pool" "general" {
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      role = "general"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_node_pool" "spot" {
+  name    = "spot"
+  cluster = google_container_cluster.primary.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      team = "devops"
+    }
+
+    taint {
+      key    = "instance_type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+
+
+
+
